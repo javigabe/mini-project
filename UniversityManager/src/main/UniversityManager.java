70e5d4e34d5608499be2e579f5dd49fea4fd651b
Alvaro Garcia Gutierrez <alvaro.garcia.gutierrez@alumnos.upm.es>
2019-10-28 19:36:11 +0100

    clase student

---
+++ UniversityManager/src/main/UniversityManager.java
@@ -0,0 +1,13 @@
+package main;
+
+public class UniversityManager {
+
+    class Student {
+    	String nombre;
+
+    }
+
+    class Course {
+        
+    }
+}
--- src/main/UniversityManager.java
+++ src/main/UniversityManager.java
@@ -4,9 +4,20 @@ public class UniversityManager {
 
     class Student {
 
+        int id;
+        String name;
+        String email;
+
+        public Student (int i, String n, String e){
+            id=i;
+            name=n;
+            email=e;
+        }
+        
     }
 
     class Course {
-        
+
+
     }
 }
