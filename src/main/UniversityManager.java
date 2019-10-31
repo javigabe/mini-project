3b0ac5f3c622b9ec8e026f5c9c6aceedbeafc01d
Javier Galarza <javier.galarza12@gmail.com>
2019-10-31 13:39:18 +0100

    Added some functions

--- src/main/UniversityManager.java
+++ src/main/UniversityManager.java
@@ -1,10 +1,37 @@
 package main;
 
 import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class UniversityManager {
     Set<Student> studentSet;
     Set<Course> courseSet;
+
+    public ArrayList<Student> matriculatedStudents(Integer code) throws Exception {
+        Course myCourse = null;
+
+        // Pick the course with code "code"
+        for (Course course: courseSet) {
+            if (course.code.equals(code)) {
+                myCourse = course;
+                break;
+            }
+        }
+
+        if (myCourse == null) throw new Exception();
+
+        myCourse.sortStudents();
+
+        return myCourse.studentsEnrrolled;
+    }
+
+    public ArrayList<Student> allUsers () {
+        ArrayList<Student> studentArrayList = new ArrayList(studentSet);
+
+
+    }
 
     class Student {
 
@@ -12,7 +39,7 @@ public class UniversityManager {
         String studentName;
         String studentEmail;
 
-        public Student (int id, String name, String email) throws  Exception{
+        public Student(int id, String name, String email) throws Exception {
             if (!email.matches("*@*[^'.']")) throw new Exception();
             studentID = id;
             studentName = name;
@@ -20,35 +47,50 @@ public class UniversityManager {
             studentSet.add(this);
         }
 
-        public void addCourse(Course course) throws Exception{
-          if (course.students < 50){
-            course.studentsEnrrolled.add(this);
-            course.students++;
-          }
-          else {
-            throw new Exception();
-          }
-
-          public void addCourse(Course course){
-          course.studentsEnrrolled.add(this);
-          course.students++;
+        public void addCourse(Course course) throws Exception {
+            if (course.students < 50) {
+                course.studentsEnrrolled.add(this);
+                course.students++;
+            }
+            else throw new Exception();
         }
-
     }
 
     class Course {
 
-        int code;
+        Integer code;
         String courseName;
         String coordinator;
         int students = 0;
-        Set<Student> studentsEnrrolled;
+        ArrayList<Student> studentsEnrrolled = new ArrayList<>();
 
         public Course(int cod, String name, String coord) {
             code = cod;
             courseName = name;
             coordinator = coord;
-            courseSet.add(this);
+        }
+
+        private void sortStudents() {
+            Collections.sort(studentsEnrrolled, codComparator);
+        }
+
+        private Comparator<Student> codComparator = new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+                return s1.studentID-s2.studentID;
+            }
+        };
+
+        private void cancel() {
+            studentsEnrrolled = new ArrayList<>();
+            students = 0;
         }
     }
+
+    private void sortStudents (ArrayList<Student> students) {
+        
+    }
+
+
+
+
 }
