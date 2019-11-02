eff5412b6eb5fcc8e5925a5051644eef528b705b
Javier Galarza <javier.galarza12@gmail.com>
2019-11-02 13:26:33 +0100

    Pom

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,10 +1,9 @@
-import org.junit.jupiter.api.*;
+import org.junit;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTests {
     @Test // JUnit test
     void testSomething () { // test name
         Integer c = new Integer (3) ; // Prefix values
-        assertEquals (7 , c ) ; // Expected results
     }
 }
