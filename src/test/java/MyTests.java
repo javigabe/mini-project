3f2a81ce07cfac8471121e33edd27071beef8333
GALARZA BECKER, JAVIER <j.galarzab@alumnos.upm.es>
2019-11-07 17:15:38 +0000

    Delete MyTests.java

--- src/test/java/MyTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class MyTests {
-    @Test // JUnit test
-    void testSomething () { // test name
-        Integer c = new Integer (3) ; // Prefix values
-    }
-}
