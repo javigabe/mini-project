f8c561a6d762b4719d60c0da8b6831490c0da745
Alvaro Garcia Gutierrez <alvaro.garcia.gutierrez@alumnos.upm.es>
2019-11-07 18:22:19 +0100

    estructure

---
+++ src/test/java/es/upm/pproject/myproject/AppTest.java
@@ -0,0 +1,30 @@
+package es.upm.pproject.myproject;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+	UniversityManager test;
+	
+	@Test
+    public void testApp() {
+        test = new UniversityManager();
+    }
+	
+	@Test
+	public void test1() {
+		
+	}
+	
+	@Test
+	public void test2(){
+		
+	}
+}
---
+++ src/test/java/es/upm/pproject/myproject/Manager.java
@@ -0,0 +1,5 @@
+package es.upm.pproject.myproject;
+
+public class Manager {
+
+}
